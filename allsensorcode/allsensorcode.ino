50db1f775296ca206fb619306791b45b1dae6ced
joelbjohn <joelbjohn2@gmail.com>
2026-05-17 13:46:50 +0530

    Sensor code for weather station

---
+++ allsensorcode/allsensorcode.ino
@@ -0,0 +1,109 @@
+#include <Wire.h>
+
+#define LDR_PIN                   0
+#define MAX_ADC_READING           1023
+#define ADC_REF_VOLTAGE           5.0
+#define REF_RESISTANCE            5030  // measure this for best results
+#define LUX_CALC_SCALAR           12518931
+#define LUX_CALC_EXPONENT         -1.405
+
+int gate=11; 
+volatile unsigned long duration=0;
+unsigned char i[5];
+unsigned int j[40];
+unsigned char value=0;
+unsigned answer=0;
+int z=0;
+int b=1;
+
+int sensor_pin = A0
+int output_value ;
+
+void setup() {
+  Serial.begin(9600);
+   Serial.println("Reading From the Sensor ...");
+   Serial.println(F("Light Sensor Test")); Serial.println("");
+   delay(2000);  
+}
+
+void loop() {
+  //lux
+  int   ldrRawData;
+  float resistorVoltage, ldrVoltage;
+  float ldrResistance;
+  float ldrLux;
+ 
+  ldrRawData = analogRead(LDR_PIN);  
+  resistorVoltage = (float)ldrRawData / MAX_ADC_READING * ADC_REF_VOLTAGE;
+  ldrVoltage = ADC_REF_VOLTAGE - resistorVoltage;
+  ldrResistance = ldrVoltage/resistorVoltage * REF_RESISTANCE;
+  ldrLux = LUX_CALC_SCALAR * pow(ldrResistance, LUX_CALC_EXPONENT);
+ 
+  Serial.print("LDR Raw Data   : "); Serial.println(ldrRawData);
+  Serial.print("LDR Voltage    : "); Serial.print(ldrVoltage); Serial.println(" volts");
+  Serial.print("LDR Resistance : "); Serial.print(ldrResistance); Serial.println(" Ohms");
+  Serial.print("LDR Illuminance: "); Serial.print(ldrLux); Serial.println(" lux");
+
+ Serial.print(" lux");
+  
+  delay(250);
+  }
+//dht11
+{
+
+ delay(1000);
+ while(1)
+ {
+  delay(1000);
+  pinMode(gate,OUTPUT);
+  digitalWrite(gate,LOW);
+  delay(20);
+  digitalWrite(gate,HIGH);
+  pinMode(gate,INPUT_PULLUP);//by default it will become high due to internal pull up
+ // delayMicroseconds(40);
+
+  
+  duration=pulseIn(gate, LOW);
+  if(duration <= 84 && duration >= 72)
+  {
+      while(1)
+      {
+        duration=pulseIn(gate, HIGH);
+        
+        if(duration <= 26 && duration >= 20){
+        value=0;}
+        
+        else if(duration <= 74 && duration >= 65){
+        value=1;}
+        
+        else if(z==40){
+        break;}
+        
+        i[z/8]|=value<<(7- (z%8));
+        j[z]=value;
+        z++;
+      }
+    }
+answer=i[0]+i[1]+i[2]+i[3];
+
+if(answer==i[4] && answer!=0)
+{
+//lcd.setCursor(7,0);
+Serial.print(i[2]);
+//lcd.setCursor(11,1);
+Serial.print(i[0]);
+}
+
+z=0;
+i[0]=i[1]=i[2]=i[3]=i[4]=0;
+ }
+}
+//soil moisture
+{
+   output_value= analogRead(sensor_pin);
+   output_value = map(output_value,550,0,0,100);
+   Serial.print("Mositure : ");
+   Serial.print(output_value);
+   Serial.println("%");
+   delay(1000);
+   }
